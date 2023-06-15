6866f8b34b048ef90d850a8f69f32ba4c648ebf9
Davies Brown <nwosudavid77.com>
2023-06-15 23:40:02 +0100

    second-biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,12 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const args = process.argv.slice(2);
-  args.sort()
-  let list = []
+  args.sort();
+  const list = [];
   for (let i = 0; i < args.length; i++) {
     if (!list.includes(args[i])) {
-      list.push(args[i])
+      list.push(args[i]);
     }
   }
-  console.log(list[list.length - 2])
+  console.log(list[list.length - 2]);
 }
