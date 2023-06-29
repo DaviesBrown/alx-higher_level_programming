15d241a2eae89f0c1f3fca85ee4bbc8cdc807b07
Davies Brown <nwosudavid77.com>
2023-06-29 16:34:05 +0100

    python-network-0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me using curl, causing the server to respond with "You got me!" in the body of the response.
+# This script makes a request to 0.0.0.0:5000/catch_me using curl, 
 curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me --output /dev/null
