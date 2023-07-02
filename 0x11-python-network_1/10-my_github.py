28495cb78f4b6a1d321bf1399ed9cb69a12f82b6
Davies Brown <nwosudavid77.com>
2023-07-02 23:23:37 +0100

    10-commits

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,12 +4,14 @@ my github
 """
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
         username = sys.argv[1]
         password = sys.argv[2]
-        url = f'https://api.github.com/users/{username}'
-        req = requests.get(url)
+        auth = HTTPBasicAuth(username, password)
+        url = f'https://api.github.com/user'
+        req = requests.get(url, auth=auth)
         res = req.json()
         print(res['id'])
