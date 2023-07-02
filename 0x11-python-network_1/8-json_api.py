3e1e5574f75f45d725fcdbf81941e5f53a80684f
Davies Brown <nwosudavid77.com>
2023-07-02 22:42:36 +0100

    request-urllib

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,20 +2,28 @@
 """
 search api
 """
-import sys
 import requests
+import sys
 
 if __name__ == '__main__':
-    letter = ''
-    if len(sys.argv) > 1:
-        letter = sys.argv[1]
-    url = f'http://0.0.0.0:5000/search_user'
-    data = {'q': letter}
-    req = requests.post(url, data=data)
-    res = req.json()
-    if req.headers.get('content-type') == 'application/json' and res:
-        print(f'[{res.id}] {res.name}')
-    elif req.headers.get('content-type') == 'application/json' and not res:
-        print('No result')
-    else:
-        print('Not a valid JSON')
+    letter = sys.argv[1] if len(sys.argv) > 1 else ''
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        response = requests.post(url, data={'q': letter})
+        response_json = response.json()
+
+        if response.status_code == 200:
+            if isinstance(response_json, dict) and \
+                 'id' in response_json and 'name' in response_json:
+                print(f"[{response_json['id']}] {response_json['name']}")
+            elif isinstance(response_json, list) and len(response_json) > 0:
+                for user in response_json:
+                    print(f"[{user['id']}] {user['name']}")
+            else:
+                print("No result")
+        elif response.status_code == 404:
+            print("No result")
+        else:
+            print(f"Error code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
