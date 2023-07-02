0ec5c1e1a5d75e7cf7f03f498297097bba073a55
Davies Brown <nwosudavid77.com>
2023-07-02 23:07:40 +0100

    request-urllib

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,13 +8,12 @@ import sys
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-
     header = {
-    "Accept: application/vnd.github+json",
-    f"Authorization: Bearer {password}",
-    "X-GitHub-Api-Version: 2022-11-28",
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f'Bearer {password}',
+    'X-GitHub-Api-Version': 2022-11-28,
     }
     url = f'https://api.github.com/users/{username}'
-    req = requests.get(url)
+    req = requests.get(url, headers=header)
     res = req.json()
     print(res['id'])
