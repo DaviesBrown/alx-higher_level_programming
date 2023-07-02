abbbe765dcc83f258cea4fb6a929fec169527d25
Davies Brown <nwosudavid77.com>
2023-07-02 22:50:21 +0100

    request-urllib

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         url = sys.argv[1]
         r = requests.get(url)
-        if r.status_code >= 400:
-            print(f'Error code: {r.status_code}')
-        elif r.status_code >= 200:
+        if r.status_code <= 400:
             print(r.text)
+        else:
+            print(f'Error code: {r.status_code}')
