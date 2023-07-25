8fb82336c676cf44729e37da76ca6c0d9350ff9f
Davies Brown <nwosudavid77.com>
2023-07-25 19:05:15 +0100

    web-scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
 const url = process.argv[2];
+
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   }
-  const result = JSON.parse(body);
-  const obj = {};
-  result.map(todo => {
-    if (todo.completed) {
-      if (obj[todo.userId]) {
-        obj[todo.userId] += 1;
-      } else {
-        obj[todo.userId] = 1;
-      }
+
+  const todos = JSON.parse(body);
+  const data = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed && data[todo.userId] === undefined) {
+      data[todo.userId] = 1;
+    } else if (todo.completed) {
+      data[todo.userId] += 1;
     }
-    return i;
   });
-  console.log(obj);
+
+  console.log(data);
 });
