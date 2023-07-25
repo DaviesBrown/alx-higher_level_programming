fba15a39f5cb428af214610271765e77a0b53621
Davies Brown <nwosudavid77.com>
2023-07-25 12:22:44 +0100

    web-scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const readme = fs.readFileSync(
-    process.argv[2], 
-    encoding='utf-8');
+const readme = fs.readFileSync(process.argv[2], 'utf-8');
 console.log(readme);
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFileSync(
-    process.argv[2], 
-    process.argv[3], 
-    encoding='utf-8');
+fs.writeFileSync(process.argv[2], process.argv[3], 'utf-8');
