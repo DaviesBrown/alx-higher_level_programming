24f81a0059dac5ffa0fac3be5497f00a5a3dc624
Davies Brown <nwosudavid77.com>
2023-07-25 11:51:48 +0100

    web-scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,6 +4,7 @@ let id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, res, body) => {
     let people = JSON.parse(body).characters;
+    console.log(people)
     let names = people.map(person => {
         request(person, (err, res, body) => {
             let p = JSON.parse(body);
